005e67a688cc1dfe21bc732f4b8cfc47a30be75d
zac Gerber <Zgerber91@gmail.com>
2020-07-23 12:12:12 -0400

    initial commit

--- nodict.py
+++ nodict.py
@@ -3,47 +3,57 @@
 Implementation of the NoDict assignment
 """
 
-__author__ = '???'
+__author__ = 'Zachary Gerber, Michael DeMory, Tiffany Mclean'
 
 
 class Node:
     def __init__(self, key, value=None):
-        self.hash = None
-        self.key = None
-        self.value = None
-        # Your code here
+        self.hash = hash(key)
+        self.key = key
+        self.value = value
         return
 
     def __repr__(self):
-        # Your code here
-        return
+        return f'{self.__class__.__name__}({self.key}, {self.value})'
 
     def __eq__(self, other):
-        # Your code here
-        return
+        if isinstance(self, other.__class__):
+            return self.key == other.key
+        else:
+            return NotImplemented
 
 
 class NoDict:
     def __init__(self, num_buckets=10):
-        self.buckets = None
-        # Your code here
+        self.buckets = [[] for _ in range(num_buckets)]
+        self.num = num_buckets
 
     def __repr__(self):
-        # Your code here
-        return
+        return '\n'.join([f'{self.__class__.__name__}.{i}:{bucket}'for i, bucket in enumerate(self.buckets)])
 
-    def add(self, key, value):
-        # Your code here
-        return
+    def add(self, key, value=None):
+        node_a = Node(key, value)
+
+        index_bucket = self.buckets[node_a.hash % self.num]
+
+        for i in index_bucket:
+            if i == node_a:
+                index_bucket.remove(i)
+                break
+        index_bucket.append(node_a)
 
     def get(self, key):
-        # Your code here
-        return
+        find_node = Node(key)
+
+        index_bucket = self.buckets[find_node.hash % self.num]
+
+        for i in index_bucket:
+            if i == find_node:
+                return i.value
+        raise KeyError(f'{key} not found')
 
     def __getitem__(self, key):
-        # Your code here
-        return
+        return self.get(key)
 
     def __setitem__(self, key, value):
-        # Your code here
-        return
+        return self.add(key, value)
